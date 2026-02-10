2ad8ee40b18a531d50714eae8f99176def1d0b48
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2026-02-10 12:53:39 +0300

    10.02: /source: GET and POST

--- main.c
+++ main.c
@@ -35,10 +35,56 @@ static void fn(struct mg_connection *c, int ev, void *ev_data)
     {
       // source operations
       printf("%.*s\n", hm->method.len, hm->method.buf);
-      if (mg_match(hm->method, mg_str("GET"), NULL))
+      if (mg_match(hm->method, mg_str("OPTIONS"), NULL))
       {
-        // MG_INFO(("GET"));
+        MG_INFO(("OPTIONS"));
         // printf("%.*s\n", hm->query.len, hm->query.buf);
+        mg_http_reply(c, 200, "Access-Control-Allow-Origin: *\r\nAccess-Control-Allow-Headers: *\r\nContent-Type: application/json\r\n", "{\"status\": 0, \"code\": \"SUCCESS\"}\n");
+      }
+      else if (mg_match(hm->method, mg_str("GET"), NULL))
+      {
+        MG_INFO(("GET"));
+        sqlite3_stmt *stmt;
+        int rc;
+        const char *sql = "SELECT id, data FROM sources;";
+        rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "Prepare hatası: %s\n", sqlite3_errmsg(db));
+            sqlite3_close(db);
+            return 1;
+        }
+
+        json_t *body = json_loads("{\"status\": 0, \"code\": \"SUCCESS\"}", 0, json_err);
+        json_t *data_field = json_array();
+        while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+            //int no = sqlite3_column_int(stmt, 0);
+            //const unsigned char *datetime = sqlite3_column_text(stmt, 1);
+            const unsigned char *id = sqlite3_column_text(stmt, 0);
+            const unsigned char *data = sqlite3_column_text(stmt, 1);
+            //printf("no        : %d\n", no);
+            //printf("datetime  : %s\n", datetime);
+            printf("id        : %s\n", id);
+            printf("data      : %s\n", data);
+            printf("-----------------------------\n");
+            json_t *item = json_loads(data, 0, json_err);
+            //char *item_str = json_dumps(item, 0);
+            //printf("%s\n", item_str);  
+            json_object_set_new(item, "id", json_string(id));
+            char *item_str = json_dumps(item, 0);
+            printf("%s\n", item_str);  
+            json_array_append_new(data_field, item);
+            char *data_str = json_dumps(data_field, 0);
+            printf("%s\n", data_str);  
+        }
+        json_object_set_new(body, "data", data_field);
+        char *body_str = json_dumps(body, 0);
+        printf("%s\n", body_str);  
+        if (rc != SQLITE_DONE) {
+            fprintf(stderr, "Step hatası: %s\n", sqlite3_errmsg(db));
+        }
+        // printf("%.*s\n", hm->query.len, hm->query.buf);
+        mg_http_reply(c, 200, "Access-Control-Allow-Origin: *\r\nAccess-Control-Allow-Headers: *\r\nContent-Type: application/json\r\n", "%s", body_str);
+        free(body_str);
       }
       else if (mg_match(hm->method, mg_str("POST"), NULL))
       {
@@ -50,13 +96,13 @@ static void fn(struct mg_connection *c, int ev, void *ev_data)
         for (int i = 0; i < SOURCE_ID_LEN; i++)
         id[i] = (i % 2 == 0) ? (rand() % 10) + '0' : (char)((int)('a') + rand() % ((int)('z') - (int)('a')));
         id[SOURCE_ID_LEN] = '\0';
-        json_object_set_new(body, "id", json_string(id));
+        //json_object_set_new(body, "id", json_string(id));
         char *body_str = json_dumps(body, 0);
         json_decref(body);
 
         char *sql = (char*)malloc((hm->body.len+250) * sizeof(char));
 
-        sprintf(sql, "INSERT INTO sources(data) VALUES('%s');", body_str);
+        sprintf(sql, "INSERT INTO sources(id, data) VALUES('%s','%s');", id, body_str);
         printf("%s", sql);
         int rc = sqlite3_exec(db, sql, 0, 0, &sqlite3_err);
         if (rc != SQLITE_OK)
@@ -64,9 +110,9 @@ static void fn(struct mg_connection *c, int ev, void *ev_data)
           fprintf(stderr, "3-SQL error: %s\n", sqlite3_err);
           sqlite3_free(sqlite3_err);
           sqlite3_close(db);
-          mg_http_reply(c, 200, "Content-Type: application/json\r\n", "{\"status\": 1, \"code\": \"ERROR_DATABASE_INSERT\"}");
+          mg_http_reply(c, 200, "Access-Control-Allow-Origin: *\r\nAccess-Control-Allow-Headers: *\r\nContent-Type: application/json\r\n", "{\"status\": 1, \"code\": \"ERROR_DATABASE_INSERT\"}");
         }
-        mg_http_reply(c, 200, "Content-Type: application/json\r\n", "{\"status\": 0, \"code\": \"SUCCESS\", \"id\": \"%s\"}\n", id);
+        mg_http_reply(c, 200, "Access-Control-Allow-Origin: *\r\nAccess-Control-Allow-Headers: *\r\nContent-Type: application/json\r\n", "{\"status\": 0, \"code\": \"SUCCESS\", \"id\": \"%s\"}\n", id);
         free(sql);
         free(body_str);
       }
@@ -105,8 +151,8 @@ int main(int argc, char *argv[])
 
   // Create table
   char *sql1 = "DROP TABLE IF EXISTS Sourcess;"
-               "CREATE TABLE IF NOT EXISTS Sources(id INTEGER PRIMARY KEY, datetime NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime')), data TEXT);";
-  //  "CREATE TABLE Sources(id INTEGER PRIMARY KEY, datetime NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime')), status TEXT, type TEXT, name TEXT, desc TEXT);";
+               "CREATE TABLE IF NOT EXISTS Sources(no INTEGER PRIMARY KEY, datetime NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime')), id TEXT NOT NULL, data TEXT);";
+  //  "CREATE TABLE Sources(no INTEGER PRIMARY KEY, datetime NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime')), status TEXT, type TEXT, name TEXT, desc TEXT);";
 
   // datetime alternative
   // 'dt1' DATETIME NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')),
