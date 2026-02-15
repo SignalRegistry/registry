c0fef433c7a484b11bc58d19b0025a039b2da123
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2026-02-15 21:56:12 +0300

    15.02: Quart app initilized

--- src/registry_api/app.py
+++ src/registry_api/app.py
@@ -3,6 +3,25 @@ Registry API
 """
 
 
+from quart import Quart, websocket
+
+app = Quart(__name__)
+
+@app.route('/')
+async def hello():
+    return 'hello'
+
+
+@app.websocket('/ws')
+async def ws():
+    while True:
+        data = await websocket.receive()
+        await websocket.send(data)
+
 def main():
+    app.run()
     # Your app logic goes here
-    print("Hello, World.")
+    # print("Hello, World.")
+
+if __name__ == "__main__":
+    main()
