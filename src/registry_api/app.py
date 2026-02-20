1c7498dd74436d403f4a1f6f4b866cc4ac23b20f
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2026-02-20 07:03:04 +0300

    20.02: server side fixing

--- src/registry_api/app.py
+++ src/registry_api/app.py
@@ -101,7 +101,7 @@ async def default():
     )
 
 
-@app.route("/sources", methods=["GET", "POST"])
+@app.route("/sources", methods=["GET", "POST", "OPTIONS"])
 async def sources():
     db = await get_db()
     db.row_factory = aiosqlite.Row
@@ -140,12 +140,46 @@ async def sources():
         return {"success": 0}, 400
 
 
-@app.route("/")
-async def hello():
-    return "hello"
+@app.route("/source", methods=["GET", "POST", "OPTIONS"])
+async def source():
+    db = await get_db()
+    db.row_factory = aiosqlite.Row
+    if request.method == "GET":
+        cursor = await db.execute("SELECT id, data FROM sources")
+        rows = await cursor.fetchall()
+
+        result = [dict(row) for row in rows]  # her satır dict olur
+        return jsonify({"success": 1, "data": result}), 200, common_headers
+    elif request.method == "POST":
+        body = await request.get_json()
+        # print(body)
+        # return {"success": 1}, 201
+        if not all(k in body for k in ("name", "type")):
+            return {"success": 0, "message": "MISSING_DATA_FIELD"}, 400
+        if sys.maxsize > 2**32:  # 64bit
+            h = blake2b(digest_size=3)
+        else:
+            h = blake2s(digest_size=3)
+        h.update(f"{body["name"]}".encode("utf-8"))
+        id = h.hexdigest()
+        try:
+            await db.execute(
+                "INSERT INTO sources(id, data) VALUES(?,?)",
+                (id, json.dumps(body, ensure_ascii="False")),
+            )
+            await db.commit()
+
+            res_headers = common_headers
+            res_headers["Location"] = f"{request.base_url}/source/{id}"
+            return (jsonify({"success": 1, "id": f"{id}"}), 201, res_headers)
+        except Exception:
+            await db.rollback()
+            return {"success": 0}, 400, common_headers
+    else:
+        return (jsonify({"success": 0}), 200, common_headers)
 
 
-@app.websocket("/ws")
+@app.websocket("/websocket")
 async def ws():
     while True:
         data = await websocket.receive()
